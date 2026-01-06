f82c9226c57d9a489cb98e5fe51f04e8f822aa0d
moha2000000 <mlouchiri007@gmail.com>
2026-01-06 14:18:36 +0100

    Amélioration: Ajout d'informations dans App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -2,10 +2,12 @@ package com.devops;
 
 /**
  * Application principale du projet DevOps
+ * Auteur: Louchiri Mohamed
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Bonjour et bon courage dans votre projet en DevOps");
+        System.out.println("Projet DevOps - CI/CD avec GitHub Actions et Jenkins");
     }
 }
 
